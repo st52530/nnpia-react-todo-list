9ee984f8cd1deeac09525d57f79956bcac26e222
Tomas Kricensky <st52530@student.upce.cz>
2020-04-06 13:43:34 +0200

    Add bootstrap

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+import Button from 'react-bootstrap/Button';
 
 function App() {
   return (
     <div className="App">
-      App
+        <Button className="button">Bootstrap button</Button>
     </div>
   );
 }
